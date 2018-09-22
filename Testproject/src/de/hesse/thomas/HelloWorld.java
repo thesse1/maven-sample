53f9660bef65fdf784958bc04bb741696c6e95a0
Thomas Hesse <hesse.thomas@gmx.de>
2018-09-22 21:22:27 +0200

    First commit

---
+++ Testproject/src/de/hesse/thomas/HelloWorld.java
@@ -0,0 +1,10 @@
+package de.hesse.thomas;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
---
+++ Testproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author THES
+ *
+ */
+module testproject {
+}
