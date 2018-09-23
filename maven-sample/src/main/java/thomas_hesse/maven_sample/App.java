62813c87e257ac4017f5bd5764c8b755d3dc5abc
Thomas Hesse <hesse.thomas@gmx.de>
2018-09-23 08:48:43 +0200

    First commit

---
+++ maven-sample/src/main/java/thomas_hesse/maven_sample/App.java
@@ -0,0 +1,13 @@
+package thomas_hesse.maven_sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
